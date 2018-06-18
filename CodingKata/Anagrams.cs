bf010aa188ea6b8b5b4f230d865247a4bc64c71d
Oana Aghebanoae <oana.aghebanoae@ness.com>
2018-06-18 23:40:40 +0300

    Created project for coding katas

---
+++ CodingKata/Anagrams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodingKata.Utils;
+
+namespace CodingKata
+{
+    // http://codekata.com/kata/kata06-anagrams/
+    public class Anagrams
+    {
+        public void FindAnagrams()
+        {
+            var wordsList = TextFileOperations.ReadAllLines(@"../../Resources/anagrams.txt");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CodingKata/Utils/TextFileOperations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingKata.Utils
+{
+    public class TextFileOperations
+    {
+        public static IEnumerable<string> ReadAllLines(string filePath)
+        {
+            IEnumerable<string> lines = new List<string>();
+
+            try
+            {
+                lines = System.IO.File.ReadAllLines(filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return lines;
+        }
+
+        public static void WriteToFile(IEnumerable<string> lines, string filePath)
+        {
+            try
+            {
+                System.IO.File.WriteAllLines(filePath, lines);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
